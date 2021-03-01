998f6fc8bf16ddd4726a599affcd50b7f0fa78a8
Ryan Birnie <ryanbirnie64@gmail.com>
2021-03-01 09:52:11 -0500

    LoginBackground

--- src/components/LoginHero.js
+++ src/components/LoginHero.js
@@ -6,17 +6,10 @@ function LoginHero() {
     return(
         
         <div className="hero-container">
-             <video src="" autoplay loop muted />   
-             <div className="hero-overlay">
-                 <img src="../img/BacgroundStraight.png" alt="background-overlay"/>
-            </div>      
-            <div className="hero-img">
-                 <img src="./img/img-home.jpg" alt="img-home"/>
-             </div>
-             
-             
-             
-             
+            {/* <video src="" autoPlay loop muted />  */}
+            <div className="hero-img"></div> 
+            <div className="overlay"></div>   
+              
              
         </div>
         
