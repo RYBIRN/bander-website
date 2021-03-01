a7d312de01b9f41303ec48c51461ad1362f471d3
Ryan Birnie <ryanbirnie64@gmail.com>
2021-03-01 12:07:50 -0500

    LoginPageBackground
    Created a responsive background layout.

--- src/components/LoginHero.js
+++ src/components/LoginHero.js
@@ -8,7 +8,11 @@ function LoginHero() {
         <div className="hero-container">
             {/* <video src="" autoPlay loop muted />  */}
             <div className="hero-img"></div> 
-            <div className="overlay"></div>   
+            <div className="overlay">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <polygon points="66 0,100 0,100 100, 25 100"/>
+            </svg>    
+            </div>   
               
              
         </div>
